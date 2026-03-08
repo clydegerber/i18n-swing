a7cf3a9c378ac56e13c090152bdd7baaea1ea222
Clyde Gerber <clyde.gerber@gmail.com>
2026-03-08 18:06:07 -0500

    Remove shell script and add it to .gitignore

--- runApp.sh
+++
@@ -1,7 +0,0 @@
-java \
---module-path target/classes:target/test-classes:\
-/Users/clydegerber/.m2/repository/dev/javai18n/i18n-core/1.2.1/i18n-core-1.2.1.jar:\
-/Users/clydegerber/.m2/repository/tools/jackson/core/jackson-databind/3.0.4/jackson-databind-3.0.4.jar:\
-/Users/clydegerber/.m2/repository/tools/jackson/core/jackson-core/3.0.4/jackson-core-3.0.4.jar:\
-/Users/clydegerber/.m2/repository/com/fasterxml/jackson/core/jackson-annotations/2.20/jackson-annotations-2.20.jar \
---module dev.javai18n.swing.test/dev.javai18n.swing.test.TestSwingApp
